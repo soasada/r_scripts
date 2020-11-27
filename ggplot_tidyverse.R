9cfef5a7caad39010cfb3d801fc5d82820eee43f
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-11-27 20:02:05 +0100

    Add mpg csv for testing purpose

--- ggplot_tidyverse.R
+++ ggplot_tidyverse.R
@@ -2,4 +2,5 @@ if ("tidyverse" %in% rownames(installed.packages()) == FALSE) {
   print("tidyverse is not installed, installing...")
   install.packages("tidyverse")
 }
-
+# Here we are using mpg for educational purpose
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
