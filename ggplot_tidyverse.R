39d12c56aef76381bb85a96c82d192c672612ded
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-11-28 20:26:45 +0100

    Bar plot script working

--- ggplot_tidyverse.R
+++ ggplot_tidyverse.R
@@ -5,6 +5,11 @@ if ("tidyverse" %in% rownames(installed.packages()) == FALSE) {
 
 library(tidyverse)
 
-cars <- read.csv("./mpg.csv")
+data_proto <- read.csv("./data/median_time.csv", dec = ",")
 
-ggplot(data = cars) + geom_point(mapping = aes(x = displ, y = hwy))
+ggplot(data_proto, aes(x = tiempo, y = proto, label = proto, fill = horas)) + 
+  geom_bar(stat="identity", position = position_dodge(width = 5)) + 
+  labs(x = "Minutos de incubación", y = "% Protoplastos transfectados", fill = "Visualización\npost-transfección",  title = "Variable tiempo de incubación", subtitle = "Promedio") +
+  geom_text(vjust = 1.6, position = position_dodge(width = 5)) +
+  theme_bw() +
+  scale_x_continuous(breaks = c(5, 10, 15, 20, 30))
