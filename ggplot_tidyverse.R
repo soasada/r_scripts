e5fb0fb901452c998d3f732f400e83697b1b838e
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-11-28 21:14:13 +0100

    Final version of the bars plot script

--- ggplot_tidyverse.R
+++ ggplot_tidyverse.R
@@ -5,11 +5,18 @@ if ("tidyverse" %in% rownames(installed.packages()) == FALSE) {
 
 library(tidyverse)
 
-data_proto <- read.csv("./data/median_time.csv", dec = ",")
+data_proto <- read.csv("./data/grafica_simple_PEG40.csv", dec = ",")
 
-ggplot(data_proto, aes(x = tiempo, y = proto, label = proto, fill = horas)) + 
-  geom_bar(stat="identity", position = position_dodge(width = 5)) + 
-  labs(x = "Minutos de incubación", y = "% Protoplastos transfectados", fill = "Visualización\npost-transfección",  title = "Variable tiempo de incubación", subtitle = "Promedio") +
-  geom_text(vjust = 1.6, position = position_dodge(width = 5)) +
+#ggplot(data_proto, aes(x = tiempo, y = proto, label = proto, fill = horas)) + 
+#  geom_bar(stat="identity", position = position_dodge(width = 5)) + 
+#  labs(x = "Minutos de incubación", y = "% Protoplastos transfectados", fill = "Visualización\npost-transfección",  title = "Variable tiempo de incubación", subtitle = "Experimento aislado") +
+#  geom_text(vjust = 1.6, position = position_dodge(width = 5)) +
+#  theme_bw() +
+#  scale_x_continuous(breaks = c(5, 10, 15, 20, 30))
+
+ggplot(data_proto, aes(x = DNA, y = proto, label = proto, fill = horas)) + 
+  geom_bar(stat="identity", position = position_dodge(width = 10)) + 
+  labs(x = "Concentración ADN (ug)", y = "% Protoplastos transfectados", fill = "Visualización\npost-transfección",  title = "Variable concentración de ADN", subtitle = "PEG 40%") +
+  geom_text(vjust = 1.6, position = position_dodge(width = 10)) +
   theme_bw() +
-  scale_x_continuous(breaks = c(5, 10, 15, 20, 30))
+  scale_x_continuous(breaks = c(10, 20, 30))
