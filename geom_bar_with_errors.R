b4825c0104198c63eab8187044bf9ccdcbb61e04
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-04-24 20:49:10 +0200

    Bars with errors

--- geom_bar_with_errors.R
+++ geom_bar_with_errors.R
@@ -5,12 +5,28 @@ if ("tidyverse" %in% rownames(installed.packages()) == FALSE) {
 
 library(tidyverse)
 
-data_proto <- read.csv("./data/promedio_PEG20.csv", dec = ",")
+data_proto <- read.csv("./data/peg40.csv", dec = ",")
+
+#ggplot(data_proto, aes(x = DNA, y = proto, label = proto, fill = horas)) + 
+ # geom_bar(stat="identity", position = position_dodge(width = 10)) + 
+  #labs(x = "Concentración ADN (ug)", y = "% Protoplastos transfectados", fill = "Visualización\npost-transfección",  title = "Variable concentración de ADN", subtitle = "PEG 40%") +
+  #geom_text(vjust = 1.6, hjust = 1.5, position = position_dodge(width = 10)) +
+  #geom_errorbar(aes(ymin=proto-DS, ymax=proto+DS), width=1, position=position_dodge(.2)) +
+  #theme_bw() +
+  #scale_x_continuous(breaks = c(10, 20, 30))
 
 ggplot(data_proto, aes(x = DNA, y = proto, label = proto, fill = horas)) + 
   geom_bar(stat="identity", position = position_dodge(width = 10)) + 
   labs(x = "Concentración ADN (ug)", y = "% Protoplastos transfectados", fill = "Visualización\npost-transfección",  title = "Variable concentración de ADN", subtitle = "PEG 40%") +
-  geom_text(vjust = 1.6, position = position_dodge(width = 10)) +
-  geom_errorbar(aes(ymin=len-sd, ymax=len+sd), width=.2, position=position_dodge(.9)) +
+  geom_errorbar(aes(ymin=proto-DS, ymax=proto+DS), width=1, position=position_dodge(width = 10)) +
+  #geom_text(vjust = 1.6, hjust = 1.5, position = position_dodge(width = 10)) +
   theme_bw() +
   scale_x_continuous(breaks = c(10, 20, 30))
+
+#ggplot(data_proto, aes(x = tiempo, y = proto, label = proto, fill = horas)) + 
+  #geom_bar(stat="identity", position = position_dodge(width = 5)) + 
+  #labs(x = "Minutos de incubación", y = "% Protoplastos transfectados", fill = "Visualización\npost-transfección",  title = "Variable tiempo de incubación", subtitle = "Promedio") +
+  #geom_text(vjust = 1.6, position = position_dodge(width = 5)) +
+  #geom_errorbar(aes(ymin=proto-DS, ymax=proto+DS), width=1, position=position_dodge(width = 5)) +
+  #theme_bw() +
+  #scale_x_continuous(breaks = c(5, 10, 15, 20, 30))
